4bc5bcc39f5fc8db62ac93da996b8fd0b2a70359
Karen V. González <karenvane1024@gmail.com>
2022-12-19 16:09:13 -0500

    Primer commit: Lista de tareas con DOM, JS, HTML +

--- script.js
+++ script.js
@@ -1,42 +1,30 @@
-import checkComplete from "./components/checkComplete.js";
-import deleteIcon from "./components/deleteIcon.js";
+import checkComplete from './components/checkComplete.js';
+import deleteIcon from './components/deleteIcon.js';
 
 const btn = document.querySelector('[data-form-btn]');
 
 const createTask = (evento) => {
-    evento.preventDefault();
-    const input = document.querySelector('[data-form-input]')
-    const value = input.value
-    const list = document.querySelector ('[data-list]')
-    const task = document.createElement('li');
-    task.classList.add('card')
-    input.value = '';
-    //backticks
-    const taskContent = document.createElement('div');
-    taskContent.appendChild(checkComplete());
-    const titleTask = document.createElement('span')
-    titleTask.classList.add('task');
-    titleTask.innerText = value;
-    taskContent.appendChild(checkComplete());
-    taskContent.appendChild(titleTask);
-   
- // task.innerHTML = content;
- task.appendChild(taskContent);
- task.appendChild(deleteIcon());
+  evento.preventDefault();
+  const input = document.querySelector('[data-form-input]');
+  const value = input.value;
+  const list = document.querySelector('[data-list]');
+  const task = document.createElement('li');
+  task.classList.add('card');
+  input.value = '';
+  //backticks
+  const taskContent = document.createElement('div');
+
+  const titleTask = document.createElement('span');
+  titleTask.classList.add('task');
+  titleTask.innerText = value;
+  taskContent.appendChild(checkComplete());
+  taskContent.appendChild(titleTask);
+  // task.innerHTML = content;
+
+  task.appendChild(taskContent);
+  task.appendChild(deleteIcon());
   list.appendChild(task);
 };
 
-//Arrow Fuctions o funciones anonimas 
-btn.addEventListener("click", createTask);
-
-const deleteIcon = () => {
-    const i = document.createElement('i')
-    i.classList.add('fas', 'fa-trash-alt', 'trashIcon', 'icon');
-    i.addEventListener('click', deleteTask);
-    return i;
-};
-
-const deleteTask= (event) =>{
-    const parent = event.target.parentElement;
-    parent.remove();
-};
+//Arrow functions o funciones anonimas
+btn.addEventListener('click', createTask);
